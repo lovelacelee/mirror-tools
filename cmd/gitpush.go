625b29e216455569855a8f54e86500442f5d5282
lovelace <lovelacelee@gmail.com>
2022-06-17 17:21:16 +0800

    :circle_arrow_right:Test push

--- cmd/gitpush.go
+++ cmd/gitpush.go
@@ -2,7 +2,7 @@
  * @Author          : Lovelace
  * @Github          : https://github.com/lovelacelee
  * @Date            : 2022-06-17 10:47:10
- * @LastEditTime    : 2022-06-17 17:17:53
+ * @LastEditTime    : 2022-06-17 17:21:11
  * @LastEditors     : Lovelace
  * @Description     : Commit the changes to the local repository and push them to multiple remotes
  * @FilePath        : /cmd/gitpush.go
@@ -37,9 +37,13 @@ func init() {
 
 func gitPush(w *git.Worktree, r *git.Repository, remote string) error {
 
-	ColorInfo("git push %s", remote)
-	err := r.Push(&git.PushOptions{RemoteName: remote})
-	ShowIfError(err)
+	// ColorInfo("git push %s", remote)
+	// err := r.Push(&git.PushOptions{RemoteName: remote})
+	// ShowIfError(err)
+
+	cmd := exec.Command("git", "push", remote)
+	RunInDir(w.Filesystem.Root(), cmd)
+
 	if verbose {
 		// Print the latest commit that was just pulled
 		ref, err := r.Head()
@@ -48,8 +52,9 @@ func gitPush(w *git.Worktree, r *git.Repository, remote string) error {
 		CheckIfError(err)
 
 		fmt.Println(latestcommit)
+		return err
 	}
-	return err
+	return nil
 }
 
 func gitPushList(w *git.Worktree, repo *git.Repository) error {
